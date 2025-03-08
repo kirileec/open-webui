b43e81d97d92a63ab8b84baa29c92bc7550f8957
kirileec <sulinke1133@gmail.com>
2025-03-08 10:50:02 +0000

    修复软删除数据导致的无法登录

--- backend/open_webui/env_linx.py
+++ backend/open_webui/env_linx.py
@@ -1,9 +1,9 @@
 import os
 
 SSO_DB_URL = os.environ.get(
-    "SSO_DB_URL", "mysql+pymysql://root:root@10.10.0.16:3306/cool"
+    "SSO_DB_URL", "mysql+pymysql://wansheng:wansheng@192.168.50.201:3306/wansheng"
 )
 SSO_SQL = os.environ.get(
     "SSO_SQL",
-    "SELECT id,user_name,email FROM chatgpt_client where user_type=2 and `status`=1 and expire_time > NOW() and email={email}",
+    "SELECT id,user_name,email FROM chatgpt_client where user_type=2 and `status`=1 and expire_time > NOW() and `deleted_at` is NULL and email={email}",
 )
